afd0f7c27aaa9e5521e48279b02d654c504d4372
sodaexe <xuanvi.tran@gmail.com>
2023-01-30 23:12:42 +0100

    add img and animations

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ gsap.to("#surface", {
     scrub: 1,
     start: "top 15%",
     end: "bottom 5%",
-    // markers: true
   }
 })
 
@@ -31,6 +30,44 @@ gsap.to("#shadow", {
     scrub: 1,
     start: "top 17%", // top de la forme, 17% de l'ecran
     end: "bottom 10%", // bottom, 10% de l'ecran
+    // markers: true
+  }
+})
+
+gsap.to('.jellyfish', {
+  scrollTrigger: {
+    trigger: '.jellyfish',
+    start: 'top 95%',
+  },
+  y: '-=850',
+  duration: 5
+})
+
+gsap.to('.fishes', {
+  duration: 10,
+  delay: 5,
+  repeat: -1,
+  motionPath: {
+    path: 'path',
+    align: 'path',
+    alignOrigin: [0.5, 0.5],
+    autoRotate: true
+  }
+})
+
+gsap.to("#rocher1", {
+  scrollTrigger: {
+    trigger: "#rocher1",
+    pin: true,
+    end: 'bottom 90vh',
     markers: true
+  }
+})
+
+gsap.to("#rocher2", {
+  scrollTrigger: {
+    trigger: "#rocher2",
+    end: 'bottom 90vh',
+    pin: true
   }
 })
